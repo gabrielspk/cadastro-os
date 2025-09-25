72b870860ab9eb778732b13ac7d454c70c494681
gabrielspk <gabrielsilvaferreira007@gmail.com>
2025-09-25 17:26:44 -0300

    feat: adiciona classe SecurityConfig

---
+++ src/main/java/com/github/gabrielspk/cadastro_os/config/SecurityConfig.java
@@ -0,0 +1,82 @@
+package com.github.gabrielspk.cadastro_os.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.github.gabrielspk.cadastro_os.security.JwtTokenFilter;
+import com.github.gabrielspk.cadastro_os.security.JwtTokenProvider;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+	@Autowired
+	private JwtTokenProvider tokenProvider;
+
+	public SecurityConfig(JwtTokenProvider tokenProvider) {
+		this.tokenProvider = tokenProvider;
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		PasswordEncoder pbkdf2Encoder = new Pbkdf2PasswordEncoder("", 8, 185000,
+				SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
+
+		Map<String, PasswordEncoder> encoders = new HashMap<>();
+		encoders.put("pbkdf2", pbkdf2Encoder);
+
+		DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("pbkdf2", encoders);
+
+		passwordEncoder.setDefaultPasswordEncoderForMatches(passwordEncoder);
+		return passwordEncoder;
+	}
+
+	@Bean
+	AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+		return configuration.getAuthenticationManager();
+	}
+
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		JwtTokenFilter filter = new JwtTokenFilter(tokenProvider);
+
+		//@formatter:off
+        return http
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(
+                                "/auth/signin",
+                                "/auth/refresh/**",
+                                "/auth/createUser",
+                                "/swagger-ui/**",
+                                "/v3/api-docs"
+                        ).permitAll()
+                        .requestMatchers("/api/**").authenticated()
+                        .requestMatchers("/users").denyAll()
+                )
+                .cors(cors -> {})
+                .build();
+        //@formatter:on
+	}
+}
